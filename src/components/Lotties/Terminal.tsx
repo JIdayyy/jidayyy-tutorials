83805bf4269094e969b61d1072f5fe7eb0484c23
JIdayyy <julien_abbadie@hotmail.fr>
2022-12-18 22:38:45 +0100

    lottie min h

--- src/components/Lotties/Terminal.tsx
+++ src/components/Lotties/Terminal.tsx
@@ -5,7 +5,7 @@ import terminalLottie from "../../Lotties/lf20_2glqweqs.json";
 const TerminalLottie = () => {
   return (
     <Lottie
-      className="invisible md:visible min-w-[400px]"
+      className="invisible md:visible min-h-[400px] min-w-[400px]"
       style={{ width: "40%" }}
       animationData={terminalLottie}
       loop
