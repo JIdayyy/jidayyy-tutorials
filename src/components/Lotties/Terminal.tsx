a34c0d63da4a2da1f978789f2d80c725b27fbfed
JIdayyy <julien_abbadie@hotmail.fr>
2022-12-18 20:19:06 +0100

    lottie file

--- src/components/Lotties/Terminal.tsx
+++ src/components/Lotties/Terminal.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable react/function-component-definition */
 import Lottie from "lottie-react";
-import terminalLottie from "../../Lotties/terminal.json";
+import terminalLottie from "../../Lotties/lf20_2glqweqs.json";
 
 const TerminalLottie = () => {
   return (
     <Lottie
-      className="invisible md:visible  min-w-[400px]"
+      className="invisible md:visible min-w-[400px]"
       style={{ width: "40%" }}
       animationData={terminalLottie}
       loop
